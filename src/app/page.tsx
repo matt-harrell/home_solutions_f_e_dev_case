99a12f96226f54cac871132737f27c88fe6a94b3
MATTHEW.H.HARRELL@GMAIL.COM <matthew.h.harrell@gmail.com>
2024-02-18 15:33:55 -0500

    add testing setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Filter from "@/components/Filter/Filter";
+
 
 export default function Home() {
   
@@ -6,7 +6,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Filter/>
+      <h1>Home</h1>
     </main>
   );
 }
